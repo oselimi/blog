f34aa06a4e6b855ff3027a3e5d0c3e461a1eda68
oselimi <selimiosman94@gmail.com>
2020-05-02 19:37:41 +0200

    Adding links between Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  get '/articles/:id', to: 'articles#show', as: :article
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
