f28f16ca7ef6e445361f3e181a5a915289f067e1
oselimi <selimiosman94@gmail.com>
2020-05-07 19:00:35 +0200

    View Article controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
+
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: :new_article
+  post '/articles', to: 'articles#create'
   get '/articles/:id', to: 'articles#show', as: :article
+
+  root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
