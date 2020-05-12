ada41d71f368b1d839ae6863fdf0c9e39e295e25
oselimi <selimiosman94@gmail.com>
2020-05-12 20:56:29 +0200

    Created Article Controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,13 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to articles_path
+    end
+  end
   private
 
   def article_params
